07aedc3f9e8ea496b096d08989a6f07bad972007
leehaedan <haedan1@naver.com>
2023-02-14 16:28:13 +0900

    0214

--- js/header-footer.js
+++ js/header-footer.js
@@ -2,7 +2,10 @@ const header = document.querySelector("#header");
 
 fetch("header.html")
     .then((res) => res.text())
-    .then((data) => (header.innerHTML = data));
+    .then((data) => {
+        header.innerHTML = data;
+        init();
+    });
 
 
 const footer = document.querySelector("#footer");
@@ -10,4 +13,16 @@ const footer = document.querySelector("#footer");
 fetch("footer.html")
     .then((res) => res.text())
     .then((data) => (footer.innerHTML = data));
+
+
+function init() {
+    const toggleBtn = document.querySelector('.navbar-toggleBtn');
+    const menu = document.querySelector('.navbar-menu');
+    const icons = document.querySelector('.navbar-icons');
+    
+    toggleBtn.addEventListener('click', () => {
+        menu.classList.toggle('active');
+        icons.classList.toggle('active');
+    });
+}
 
